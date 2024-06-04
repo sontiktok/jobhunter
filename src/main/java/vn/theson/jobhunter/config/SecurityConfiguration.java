95683a7abaf1fd645ba0abac34fa15f53da76fb1
sontiktok <sonvltiktok@gmail.com>
2024-06-04 14:20:38 +0700

    #Security - api /login, create accesset token

--- src/main/java/vn/theson/jobhunter/config/SecurityConfiguration.java
+++ src/main/java/vn/theson/jobhunter/config/SecurityConfiguration.java
@@ -1,23 +1,31 @@
 package vn.theson.jobhunter.config;
 
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import com.nimbusds.jose.util.Base64;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-
+import vn.theson.jobhunter.util.SecurityUtil;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableMethodSecurity(securedEnabled = true)
 public class SecurityConfiguration {
+
+    @Value("${jwt.base64-secret}")
+    private String jwtKey;
+
+    @Value("${jwt.token-validity-in-seconds}")
+    private long jwtExpiration;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -38,6 +46,15 @@ public class SecurityConfiguration {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
         return http.build();
 
+    }
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        return new NimbusJwtEncoder(new ImmutableSecret<>(getSecretKey()));
+    }
+    private SecretKey getSecretKey() {
+        byte[] keyBytes = Base64.from(jwtKey).decode();
+        return new SecretKeySpec(keyBytes, 0, keyBytes.length, SecurityUtil.JWT_ALGORITHM.getName());
     }
 
 }
--- src/main/java/vn/theson/jobhunter/controller/AuthController.java
+++ src/main/java/vn/theson/jobhunter/controller/AuthController.java
@@ -9,18 +9,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import vn.theson.jobhunter.entity.dto.LoginDTO;
+import vn.theson.jobhunter.entity.dto.ResLoginDTO;
+import vn.theson.jobhunter.util.SecurityUtil;
 
 @RestController
 public class AuthController {
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
+    private final SecurityUtil securityUtil;
 
-    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder) {
+
+    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder, SecurityUtil securityUtil) {
         this.authenticationManagerBuilder = authenticationManagerBuilder;
+        this.securityUtil = securityUtil;
     }
 
-
     @PostMapping("/login")
-    public ResponseEntity<LoginDTO> login(@Valid @RequestBody LoginDTO loginDTO) {
+    public ResponseEntity<ResLoginDTO> login(@Valid @RequestBody LoginDTO loginDTO) {
         // Nap input gom username/password vao Security
         UsernamePasswordAuthenticationToken authenticationToken
                 = new UsernamePasswordAuthenticationToken(loginDTO.getUsername(), loginDTO.getPassword());
@@ -28,6 +32,12 @@ public class AuthController {
         //Xac thuc nguoi dung
         Authentication authentication =
                 authenticationManagerBuilder.getObject().authenticate(authenticationToken);
-        return ResponseEntity.ok().body(loginDTO);
+
+        //Tao token
+        String accessToken = this.securityUtil.createToken(authentication);
+
+        ResLoginDTO res = new ResLoginDTO();
+        res.setAccessToken(accessToken);
+        return ResponseEntity.ok().body(res);
     }
 }
