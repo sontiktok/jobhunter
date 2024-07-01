00156408e34667ed9046b494c683e71be88050b2
sontiktok <sonvltiktok@gmail.com>
2024-07-01 17:17:10 +0700

    API login v1

--- src/main/java/vn/theson/jobhunter/controller/AuthController.java
+++ src/main/java/vn/theson/jobhunter/controller/AuthController.java
@@ -10,20 +10,24 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import vn.theson.jobhunter.entity.User;
 import vn.theson.jobhunter.entity.dto.LoginDTO;
 import vn.theson.jobhunter.entity.dto.ResLoginDTO;
+import vn.theson.jobhunter.service.UserService;
 import vn.theson.jobhunter.util.SecurityUtil;
 
 @RestController
 @RequestMapping("/api/v1")
 public class AuthController {
+
     private final AuthenticationManagerBuilder authenticationManagerBuilder;
     private final SecurityUtil securityUtil;
+    private final UserService userService;
 
-
-    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder, SecurityUtil securityUtil) {
+    public AuthController(AuthenticationManagerBuilder authenticationManagerBuilder, SecurityUtil securityUtil,UserService userService) {
         this.authenticationManagerBuilder = authenticationManagerBuilder;
         this.securityUtil = securityUtil;
+        this.userService = userService;
     }
 
     @PostMapping("/login")
@@ -41,6 +45,16 @@ public class AuthController {
         SecurityContextHolder.getContext().setAuthentication(authentication);
 
         ResLoginDTO res = new ResLoginDTO();
+
+        User currentUserDB = this.userService.handleGetUserByUsername(loginDTO.getUsername());
+        if (currentUserDB != null) {
+            ResLoginDTO.UserLogin userLogin = new ResLoginDTO.UserLogin(
+                    currentUserDB.getId(),
+                    currentUserDB.getEmail(),
+                    currentUserDB.getName());
+            res.setUser(userLogin);
+        }
+
         res.setAccessToken(accessToken);
         return ResponseEntity.ok().body(res);
     }
--- src/main/java/vn/theson/jobhunter/entity/dto/ResLoginDTO.java
+++ src/main/java/vn/theson/jobhunter/entity/dto/ResLoginDTO.java
@@ -1,14 +1,24 @@
 package vn.theson.jobhunter.entity.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class ResLoginDTO {
-
     private String accessToken;
+    private UserLogin user;
 
-    public String getAccessToken() {
-        return accessToken;
+    @Getter
+    @Setter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class UserLogin {
+        private long id;
+        private String email;
+        private String name;
     }
 
-    public void setAccessToken(String accessToken) {
-        this.accessToken = accessToken;
-    }
 }
